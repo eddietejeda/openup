ea394c949a89de64250e65b76fa6c1d1c8f51285
Eddie A Tejeda <eddie@codeforamerica.org>
2012-06-09 14:51:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenUp::Application.config.secret_token = 'b9cc87daa4ca6a14ad8e54989773209300e3acc019ddb33bb09a9eb83149247e452e18abfac7b928f1c2796ac0f07b10d4245a1e15d2016ae42dd98ac47b6989'
