f55dac8f7856ebe441834278a13fbb4d655d2629
adamjacobbecker <ad@mbecker.cc>
2012-06-10 10:24:17 -0400

    show request status

--- app/models/request.rb
+++ app/models/request.rb
@@ -20,6 +20,10 @@ class Request < ActiveRecord::Base
   has_many :responses, dependent: :destroy
   
   before_save :create_key
+  
+  def status
+    self.responses.last.status if self.responses.any?
+  end
 
   validates :department_id, presence: true
   validates :subject, presence: true
