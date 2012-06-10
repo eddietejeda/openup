a4ddcd316d696b3f518c2efec6e485b85ff3c949
adamjacobbecker <ad@mbecker.cc>
2012-06-09 22:34:25 -0400

    removing redundant column in requests

--- test/unit/request_test.rb
+++ test/unit/request_test.rb
@@ -2,15 +2,14 @@
 #
 # Table name: requests
 #
-#  id             :integer         not null, primary key
-#  department_id  :integer
-#  response_key   :string(255)
-#  body           :text
-#  responded_date :string(255)
-#  created_at     :datetime        not null
-#  updated_at     :datetime        not null
-#  subject        :string(255)
-#  requester_id   :integer
+#  id            :integer         not null, primary key
+#  department_id :integer
+#  response_key  :string(255)
+#  body          :text
+#  created_at    :datetime        not null
+#  updated_at    :datetime        not null
+#  subject       :string(255)
+#  requester_id  :integer
 #
 
 require 'test_helper'
