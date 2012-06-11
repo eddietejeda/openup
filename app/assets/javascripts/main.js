450b92c1442defaf79effae759e67c48636ff973
adamjacobbecker <ad@mbecker.cc>
2012-06-11 17:52:05 -0400

    shrink header on scroll

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,8 +2,22 @@ function goToByScroll(id){
   $('html,body').animate({scrollTop: $("#"+id).offset().top - 155},'slow');
 }
 
+function resizeHeader(){
+    var pos = $(window).scrollTop();
+    
+    if (pos >= 60) {
+        $(".header").addClass('fixed')
+    }else{
+        $(".header").removeClass('fixed')        
+    }
+}
+
 $(function(){
     $("[data-scroll-to]").click(function(){
        goToByScroll($(this).data('scroll-to')); 
-    });    
+    });   
+    
+    $(window).scroll(resizeHeader);
+    
+    resizeHeader();
 })
